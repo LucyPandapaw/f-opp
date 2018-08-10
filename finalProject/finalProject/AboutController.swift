9b344f9cb464159652cf832450177dca8a381d57
meme <38194992+LucyPandapaw@users.noreply.github.com>
2018-08-10 16:41:56 -0700

    Add files via upload

---
+++ finalProject/finalProject/AboutController.swift
@@ -0,0 +1,9 @@
+//
+//  AboutController.swift
+//  finalProject
+//
+//  Created by Fangjian Shang on 8/10/18.
+//  Copyright © 2018 Fangjian Shang. All rights reserved.
+//
+
+import Foundation
---
+++ finalProject/finalProject/DailyCalorieController.swift
@@ -0,0 +1,9 @@
+//
+//  DailyCalorieController.swift
+//  finalProject
+//
+//  Created by Fangjian Shang on 8/10/18.
+//  Copyright © 2018 Fangjian Shang. All rights reserved.
+//
+
+import Foundation
